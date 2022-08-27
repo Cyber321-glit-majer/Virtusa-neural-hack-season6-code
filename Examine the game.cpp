496e786ce180726f4537f852df56144a9aff0060
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-08-27 14:58:19 +0530

    Create Examine the game.cpp

---
+++ Examine the game.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int count(int n)
+{
+	int table[n+1], i;
+
+	memset(table, 0, sizeof(table));
+
+	table[0] = 1;
+
+	for (i=3; i<=n; i++)
+	table[i] += table[i-3];
+	for (i=5; i<=n; i++)
+	table[i] += table[i-5];
+	for (i=10; i<=n; i++)
+	table[i] += table[i-10];
+
+	return table[n];
+}
+
+
+// Driver program
+int main(void)
+{
+	int n;
+	cin>>n;
+	cout<<count(n);
+	
+	return 0;
+}
